82d30a14bf7c84b16502ea09fead26ff317c3e6a
21karat <1252515809@qq.com>
2019-04-03 15:22:24 +0800

    整理

---
+++ src/main/java/po/User.java
@@ -0,0 +1,19 @@
+package po;
+/**
+ * vo
+ * @author 开发
+ *
+ */
+public class User {
+	
+	Long uid;
+	
+	public Long getUid() {
+		return uid;
+	}
+
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	
+}
